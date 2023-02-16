30da9dc3d75f54be4e8b5f1cfdbe79683ef3223b
Richard Ngo <richard1ngo@gmail.com>
2023-02-15 21:48:41 -0500

    add files missed from feedback, bio comment html form complete, add shows hero

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,22 @@
+
+// //Shows Hero
+// const getHero = document.getElementsByTagName('div');
+// getHero[2].append(getHero[2].classList.add('hero'));
+// getHero[2].style.display = 'flex';
+// getHero[2].style.backgroundImage = 'linear-gradient(rgba(32,32,32, 0.5), rgba(32, 32, 32, 0.5)),url(../assets/Images/hero-shows.jpg)';
+// getHero[2].style.backgroundSize = 'cover';
+// getHero[2].style.backgroundPosition = 'center';
+// getHero[2].style.width = '100%';
+// getHero[2].style.height = '60vw';
+// getHero[2].style.display = 'flex';
+
+// const getHeroAlbum = document.getElementsByTagName('h2');
+// getHeroAlbum[0].append(getHeroAlbum[0].classList.add('hero__album'));
+// getHeroAlbum[0].innerText = 'Beatiful Beasts Album';
+
+// const getHeroTitle = document.getElementsByTagName('h1');
+// getHeroTitle[0].append(getHeroTitle[0].classList.add('hero__title'));
+// getHeroTitle[0].innerText = 'Queen of Yellow x For The Stings';
+// console.log(getHeroTitle);
+
+// //Functions
