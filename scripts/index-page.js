33eeafd6a6dca3be3c633559418008889ec9ebec
Richard Ngo <richard1ngo@gmail.com>
2023-02-19 13:55:41 -0500

    complete style, reset, aligning of comment section on index.html, begin style work for shows.html page. concept for Javascript done on paper

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,147 +1,209 @@
 const commentLog = [
     {
         name: 'Sachie Sharma',
-        date: '02/18/2023',
-        comment: 'This code definitely works.'
+        date: '02/18/2021',
+        message: 'This code definitely works. Pizza ipsum dolor amet chicken wing white pizza ham mushrooms Chicago style NY style green bell peppers pineapple mayo thin crust ranch extra cheese. Ranch lasagna personal extra cheese pie mayo pan garlic parmesan.'
     },
     {
         name: 'Richard Ngo',
-        date: '02/17/2023',
-        comment: 'I wish this code worked.'
+        date: '02/17/2022',
+        message: 'I wish this code worked. We don’t serve their kind here! What? Your droids. They’ll have to wait outside. We don’t want them here. Listen, why don’t you wait out by the speeder. We don’t want any trouble. I heartily agree with you sir.'
+    },
+    {
+        name: 'Darth Vader',
+        date: '02/19/2023',
+        message: 'Nooooooooooooooo. Daijobu kimochi damaru arigatou fuzakeru Oniisan Ureshiii Otaku damasu. Doki doki chikara Senpai Daijobu Oniisan damaru chikara. Daijobu Kawaii Baka Daijobu okasan arigatou Ureshiii.'
     }
 ];
 
-document.querySelector(".comment__form").addEventListener("submit", function(event){
+displayComment();
+formDate();
+
+const commentForm = document.querySelector(".comment__form");
+
+commentForm.addEventListener("submit", function(event){
     event.preventDefault();
 
     let name = document.getElementById("name").value;
     let date = document.getElementById("date").value;
-    let comment = document.getElementById("comment").value;
+    let message = document.getElementById("message").value;
 
     let newComment = {
         name: name,
         date: date,
-        comment: comment
+        message: message
     };
-
     commentLog.push(newComment);
-
+    commentForm.reset();
     displayComment();
 });
 
-// // Append label and input elements to form, then append form to container
-// form.appendChild(nameLabel);
-// form.appendChild(nameInput);
-// form.appendChild(commentLabel);
-// form.appendChild(commentInput);
-// form.appendChild(submitButton);
-// container.appendChild(profile);
-// container.appendChild(form);
-
-// // Create first divider element
-// const divider1 = document.createElement("hr");
-// divider1.classList.add("comment__divider");
-
-// // Create second container element for comment entry
-// const container2 = document.createElement("div");
-// container2.classList.add("comment__container", "margin");
-// const profile2 = document.createElement("div");
-// profile2.classList.add("comment__profile");
-// const profileImage2 = document.createElement("img");
-// profileImage2.classList.add("comment__image");
-// profileImage2.src = "#";
-// profileImage2.alt = "profile pic";
-// profile2.appendChild(profileImage2);
-// const entryHeader = document.createElement("div");
-// entryHeader.classList.add("comment__entryheader");
-// const name = document.createElement("p");
-// name.classList.add("comment__name");
-// name.textContent = "Richard Ngo";
-// const timecode = document.createElement("p");
-// timecode.classList.add("comment__timecode");
-// timecode.textContent = "02/17/2023";
-// entryHeader.appendChild(name);
-// entryHeader.appendChild(timecode);
-// const text = document.createElement("p");
-// text.classList.add("comment__text");
-// text.textContent = "Wish this code worked. Pizza ipsum do";
-
-let sectionAll = document.querySelector("section.comment.margin");
-console.log(sectionAll);
 
 function formDate(){
-    // Get today's date
-var d = new Date();
-var day = d.getDate();
-var month = d.getMonth() + 1; // The months are 0-based
-var year = d.getFullYear();
-// Prepend the day and month with 0
-// Comment this out/remove it, to use the day and month as-is
-if (day < 10) {
-    day = "0" + day;
-  }
-  if (month < 10) {
-    month = "0" + month;
-  }
-// Set the date field to the current date
-console.log(document.getElementById("date").value = month + "/" + day + "/" + year);
-return document.getElementById("date").value = month + "/" + day + "/" + year;
 
-};
-
-console.log(formDate());
-
-/* <div class="comment__new margin">
-<hr class="comment__divider">
-<div class="comment__container">
-    <div class="comment__profile">
-        <img class="comment__image" src="#" alt="profile pic">
-    </div>
-    <div class="comment__container--entry">
-        <div class="comment__entryheader">
-            <p class="comment__name">Richard Ngo</p>
-            <p class="comment__timecode">02/17/2023</p>
-        </div>
-        <p class="comment__text">Wish this code worked. Pizza ipsum dolor amet lasagna pan sausage chicken
-             and bacon garlic parmesan white pizza. Meat lovers large pizza roll, thin crust bacon red
-             onions hand tossed. Garlic sauce melted cheese banana peppers stuffed red onions garlic.
-             Stuffed crust extra cheese ranch chicken and bacon, lasagna philly steak chicken. Red onions
-             large deep crust, melted cheese bbq rib chicken and bacon marinara mushrooms onions pizza roll
-             bacon ham white garlic green bell peppers. Beef garlic sauce mushrooms personal.</p>
-    </div>
-</div>
-<hr class="comment__divider">
-</div> */
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth() + 1;
+    var year = d.getFullYear();
+    
+    if (day < 10) {
+        day = "0" + day;
+      }
+      if (month < 10) {
+        month = "0" + month;
+      }
+    
+    console.log(document.getElementById("date").value = month + "/" + day + "/" + year);
+    return document.getElementById("date").value = month + "/" + day + "/" + year;
+    
+    };
 
 function displayComment(){
-    let displayNewComment = document.querySelector(".comment__new margin");
+    let displayNewComment = document.querySelector(".comment__new");
+    console.log(displayNewComment);
     displayNewComment.innerHTML = "";
+    let dividerCounter = 0;
 
-    for (let i = 0; i < commentLog.length; i++) {
+    for (let i = commentLog.length - 1; i >= 0; i--) {
         let newComment = commentLog[i];
 
-        const container = document.createElement("div");
-        container.classList.add("comment__container");
-        const profile = document.createElement("div");
-        profile.classList.add("comment__profile");
-        const profileImage = document.createElement("img");
+        if(dividerCounter === 0){
+        dividerCounter++;    
+        const topDivider = document.createElement("hr");
+        topDivider.classList.add("comment__divider");
+        displayNewComment.appendChild(topDivider);
+    }
+
+        const commentContainerDiv = document.createElement("div");
+        commentContainerDiv.classList.add("comment__container");
+        displayNewComment.appendChild(commentContainerDiv);
+
+        const profileDiv = document.createElement("div");
+        profileDiv.classList.add("comment__profile");
+        commentContainerDiv.appendChild(profileDiv);
+        const profileImage = document.createElement("p");
         profileImage.classList.add("comment__image");
-        profileImage.src = "#";
-        profileImage.alt = "profile pic";
-        profile.appendChild(profileImage);
-        const entryHeader = document.createElement("div");
-        entryHeader.classList.add("comment__entryheader");
-        const name = document.createElement("p");
-        name.classList.add("comment__name");
-        name.textContent = "Richard Ngo";
-        const timecode = document.createElement("p");
-        timecode.classList.add("comment__timecode");
-        timecode.textContent = "02/17/2023";
-        entryHeader.appendChild(name);
-        entryHeader.appendChild(timecode);
-        const text = document.createElement("p");
-        text.classList.add("comment__text");
-        text.textContent = newComment.comment;
+        profileImage.setAttribute = ("alt", "profile pic");
+        profileDiv.appendChild(profileImage);
+
+        const entryContainerDiv = document.createElement("div");
+        entryContainerDiv.classList.add("comment__container--entry");
+        commentContainerDiv.appendChild(entryContainerDiv);
+
+        const entryHeaderDiv = document.createElement("div");
+        entryHeaderDiv.classList.add("comment__entryheader");
+        entryContainerDiv.appendChild(entryHeaderDiv);
+
+        const nameParam = document.createElement("p");
+        nameParam.classList.add("comment__name");
+        nameParam.innerText = newComment.name;
+        entryHeaderDiv.appendChild(nameParam);
+
+        const dateParam = document.createElement("p");
+        dateParam.classList.add("comment__timecode");
+        dateParam.innerText = newComment.date;
+        entryHeaderDiv.appendChild(dateParam);
+
+        const messageParam = document.createElement("p");
+        messageParam.classList.add("comment__text");
+        messageParam.innerText = newComment.message;
+        entryContainerDiv.appendChild(messageParam);
+
+        const bottomDivider = document.createElement("hr");
+        bottomDivider.classList.add("comment__divider");
+        displayNewComment.appendChild(bottomDivider);
 
     }
 };
+
+// function formDate(){
+
+// var d = new Date();
+// var day = d.getDate();
+// var month = d.getMonth() + 1;
+// var year = d.getFullYear();
+
+// if (day < 10) {
+//     day = "0" + day;
+//   }
+//   if (month < 10) {
+//     month = "0" + month;
+//   }
+
+// console.log(document.getElementById("date").value = month + "/" + day + "/" + year);
+// return document.getElementById("date").value = month + "/" + day + "/" + year;
+
+// };
+
+/* <div class="comment__container">
+<div class="comment__profile">
+    <p class="comment__image" alt="profile pic"></p>
+</div>
+<div class="comment__container--entry">
+    <div class="comment__entryheader">
+        <p class="comment__name">Richard Ngo</p>
+        <p class="comment__timecode">02/17/2023</p>
+    </div>
+    <p class="comment__text">Wish this code worked. Pizza ipsum. Pizza ipsum. Pizza ipsum. Pizza ipsum. Pizza ipsum.</p>
+</div>
+</div>
+<hr class="comment__divider"> */
+
+// function displayComment(){
+//     let displayNewComment = document.querySelector(".comment__new");
+//     console.log(displayNewComment);
+//     displayNewComment.innerHTML = "";
+//     let dividerCounter = 0
+//     for (let i = 0; i < 0; i++) {
+//         let newComment = commentLog[i];
+
+//         if(dividerCounter === 0){
+//         dividerCounter++;    
+//         const topDivider = document.createElement("hr");
+//             topDivider.classList.add("comment__divider");
+//             displayNewComment.appendChild(topDivider);
+//     }
+
+//         const commentContainerDiv = document.createElement("div");
+//         commentContainerDiv.classList.add("comment__container");
+//         displayNewComment.appendChild(commentContainerDiv);
+
+
+
+//         const profileDiv = document.createElement("div");
+//         profileDiv.classList.add("comment__profile");
+//         commentContainerDiv.appendChild(profileDiv);
+//         const profileImage = document.createElement("p");
+//         profileImage.classList.add("comment__image");
+//         profileImage.setAttribute = ("alt", "profile pic");
+//         profileDiv.appendChild(profileImage);
+
+//         const entryContainerDiv = document.createElement("div");
+//         entryContainerDiv.classList.add("comment__container--entry");
+//         commentContainerDiv.appendChild(entryContainerDiv);
+
+//         const entryHeaderDiv = document.createElement("div");
+//         entryHeaderDiv.classList.add("comment__entryheader");
+//         entryContainerDiv.appendChild(entryHeaderDiv);
+
+//         const nameParam = document.createElement("p");
+//         nameParam.classList.add("comment__name");
+//         nameParam.innerText = newComment.name;
+//         entryHeaderDiv.appendChild(nameParam);
+
+//         const dateParam = document.createElement("p");
+//         dateParam.classList.add("comment__timecode");
+//         dateParam.innerText = newComment.date;
+//         entryHeaderDiv.appendChild(dateParam);
+
+//         const messageParam = document.createElement("p");
+//         messageParam.classList.add("comment__text");
+//         messageParam.innerText = newComment.message;
+//         entryContainerDiv.appendChild(messageParam);
+
+//         const bottomDivider = document.createElement("hr");
+//         bottomDivider.classList.add("comment__divider");
+//         displayNewComment.appendChild(bottomDivider);
+
+//     }
+// };
