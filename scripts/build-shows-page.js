ebd2f72289780db6ae9d504fae66e80dcf5c2017
Richard Ngo <richard1ngo@gmail.com>
2023-02-19 21:16:33 -0500

    shows page style nearly complete. js to be done.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,4 +1,35 @@
-
+const showList = [
+    {
+        date: 'Mon Sept 06 2023',
+        venue: 'Ronald Lane',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Tue Sept 21 2023',
+        venue: 'Pier 3 East',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Fri Oct 15 2023',
+        venue: 'View Lounge',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Sat Nov 06 2023',
+        venue: 'Hyatt Agency',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Fri Nov 26 2023',
+        venue: 'Moscow Center',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Wed Dec 15 2023',
+        venue: 'Press Club',
+        location: 'San Francisco, CA'
+    }
+];
 // //Shows Hero
 // const getHero = document.getElementsByTagName('div');
 // getHero[2].append(getHero[2].classList.add('hero'));
